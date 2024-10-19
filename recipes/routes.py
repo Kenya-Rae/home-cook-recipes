53dc43c6ae97a8ab2e0622624dba77259617e4d2
Kenya Francis <kenyarae99@gmail.com>
2024-10-19 13:39:22 +0000

    Add create function

--- recipes/routes.py
+++ recipes/routes.py
@@ -11,7 +11,9 @@ def index():
 
 @app.route("/recipes")
 def recipes():
-    return render_template("recipes.html")
+    all_recipes = Recipes.query.all()
+
+    return render_template("recipes.html", recipes=all_recipes)
 
 
 @app.route("/sign_up", methods=["GET", "POST"])
@@ -86,7 +88,11 @@ def dashboard():
         flash('Please sign in.')
         return redirect(url_for('signin'))
 
-    return render_template('dashboard.html')
+    user = Users.query.filter_by(email=session["email"]).first()
+
+    user_recipes = Recipes.query.filter_by(user_id=user.id).all()
+
+    return render_template('dashboard.html', user=user, recipes=user_recipes)
     
 
 @app.route("/logout")
@@ -97,9 +103,52 @@ def logout():
     return redirect(url_for("signin"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
-    return render_template("add_recipe.html")
+    if 'email' not in session:
+        flash('Please sign in.')
+        return redirect(url_for('signin'))
+
+    user = Users.query.filter_by(email=session["email"]).first()
+
+    if request.method == "POST":
+        title = request.form.get("recipe_name")
+        description = request.form.get("recipe_description")
+        instructions = request.form.get("recipe_instructions")
+        prep_time = request.form.get("preptime")
+        cook_time = request.form.get("cooktime")
+        servings = request.form.get("servings")
+
+        new_recipe = Recipes(
+            title=title,
+            description=description,
+            instructions=instructions,
+            prep_time=prep_time,
+            cook_time=cook_time,
+            total_time=int(prep_time) + int(cook_time),  # total time
+            servings=servings,
+            user_id=user.id  # Link recipe to the logged-in user
+        )
+
+        # Add the new recipe to the session and commit
+        db.session.add(new_recipe)
+        db.session.commit()
+        return redirect(url_for("dashboard"))
+
+        categories = Category.query.all()
+
+    return render_template("add_recipe.html", categories=categories)
+
+
+@app.route("/recipe/<int:recipe_id>")
+def view_recipe(recipe_id):
+    # Get the recipe by ID
+    recipe = Recipes.query.get_or_404(recipe_id)
+
+    # Query category
+    category = recipe.categories
+
+    return render_template("view_recipe.html", recipe=recipe, category=category)
 
 
 @app.route("/gallery")
